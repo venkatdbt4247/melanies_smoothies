5db238f4678aef44adafe0c6aab600bf2aa595ae
venkatdbt4247 <119379661+venkatdbt4247@users.noreply.github.com>
2025-02-04 15:34:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,7 @@ st.write(
 
 name_on_order = st.text_input ("Name on Smoothie:")
 # cnx = st.connection("snowflake")
-cnx = st.connection(
-    "snowflake",
-    user="RAYALAVENKAT4247",
-    password="Authority8998",
-    account="TOGEXEZ-MX28295",
-    warehouse="COMPUTE_WH",
-    database="SMOOTHIES",
-    schema="PUBLIC"
-)
+cnx = st.connection( "snowflake")
 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
